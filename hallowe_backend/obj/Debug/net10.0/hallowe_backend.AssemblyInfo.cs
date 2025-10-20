d10109000848f57b4a534afcfa7c75c2a4c8eaf5
therealdanmalmx <danmalmx@gmail.com>
2025-10-20 22:00:45 +0200

    Add neon postgre connection string

--- hallowe_backend/obj/Debug/net10.0/hallowe_backend.AssemblyInfo.cs
+++ hallowe_backend/obj/Debug/net10.0/hallowe_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hallowe_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85955b0a9e80ddf41799a005dddcf68242da8420")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ec29d0b0fcc0c03afbf486bf260cc655a141cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("hallowe_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hallowe_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
