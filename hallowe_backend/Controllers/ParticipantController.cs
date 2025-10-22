a6ab467c9325a11e37202457671bcd3a9e875ea5
therealdanmalmx <danmalmx@gmail.com>
2025-10-22 08:47:11 +0200

    [WIP] Get participants.

--- hallowe_backend/Controllers/ParticipantController.cs
+++ hallowe_backend/Controllers/ParticipantController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using hallowe_backend.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace hallowe_backend.Controllers
@@ -10,6 +11,12 @@ namespace hallowe_backend.Controllers
     [Route("api/[controller]")]
     public class ParticipantController : ControllerBase
     {
-        
+
+        [HttpGet(Name = "GetParticipants")]
+        public IEnumerable<Participant> Get()
+        {
+            // TODO: replace with your real data source (e.g., DB or repository)
+            return new List<Participant>();
+        }
     }
 }
