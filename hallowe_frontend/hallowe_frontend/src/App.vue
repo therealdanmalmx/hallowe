827cb318c02553472c9ba0715256a5fa4bf4fee6
Bella Solomon-Falkfrii - CDA24 GBG <bella.solomon-falkfrii@iths.se>
2025-10-31 10:55:10 +0100

    app styling

--- hallowe_frontend/hallowe_frontend/src/App.vue
+++ hallowe_frontend/hallowe_frontend/src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
   import Navbar from './components/NavbarComp.vue'
-  import { RouterView, } from 'vue-router'
+  import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="py-10">
-    <RouterView />
-    <Navbar />
-  </div>
+  <RouterView />
+  <Navbar />
 </template>
