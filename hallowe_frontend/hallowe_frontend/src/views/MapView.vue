670e2a9307de9e5a8ecb3d6531ccedb2865fe64a
therealdanmalmx <danmalmx@gmail.com>
2025-11-06 10:20:23 +0100

    Update interface

--- hallowe_frontend/hallowe_frontend/src/views/MapView.vue
+++ hallowe_frontend/hallowe_frontend/src/views/MapView.vue
@@ -4,16 +4,11 @@
   import MapComp from '../components/MapComp.vue'
   // import { useLoading } from 'vue-loading-overlay';
   import RotateLoader from 'vue-spinner/src/RotateLoader.vue'
-import { userService } from '../api/services/participantServices';
+  import { userService } from '../api/services/participantServices';
+  import type { Participant } from '../types/interfaces';
 
   const mapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
-  interface House {
-    name: string,
-    city: string
-    streetName: string,
-    postalCode: string
-  }
 
   export default defineComponent({
     name: 'MapView',
@@ -22,7 +17,7 @@ import { userService } from '../api/services/participantServices';
       const defaultCenter = { lat: 57.725, lng: 13.162 }
       const zoom = ref(7)
       let isLoading = ref<boolean>(false);
-      let data = ref<House[]>([]);
+      let data = ref<Participant[]>([]);
       const color = ref<string>("#FF7518");
       const size = ref('1.25rem');
 
@@ -37,12 +32,16 @@ import { userService } from '../api/services/participantServices';
         // loading.show()
         try {
 
-          userService.getAll();
+          const userResponse = await userService.getAll();
           // const res = await fetch('http://localhost:5168/api/Participant')
           const response = await fetch(
             `https://maps.googleapis.com/maps/api/js?key=${mapsApiKey}&libraries=places`
           )
-          
+
+          if (userResponse) {
+            data.value = await userResponse.data;
+            console.log('Fetched data:', data)
+          }
           if (response.ok) {
             const mapData = await response.json()
             console.log('Fetched map:', mapData)
@@ -75,8 +74,8 @@ import { userService } from '../api/services/participantServices';
   <div class="h-screen flex items-center justify-center" v-if="isLoading"><rotate-loader :loading="isLoading" :color="color" :size="size"></rotate-loader></div>
   <div v-else class="py-4">
       <MapComp :defaultCenter="defaultCenter" :zoom="zoom" />
-      <div v-for="house in data">
-        <p>{{ house.name }} | {{ house.streetName }} | {{ house.postalCode }} | {{ house.city }}</p>
+      <div v-for="participant in data">
+        <p>{{ participant.name }} | {{ participant.streetName }} | {{ participant.streetNumber }} | {{ participant.postalCode }} | {{ participant.city }}</p>
       </div>
   </div>
 </template>
