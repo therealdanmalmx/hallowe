180be0e4f7fc8ab0814e4a3cb07a06cd914ac02a
therealdanmalmx <danmalmx@gmail.com>
2026-01-08 17:41:46 +0100

    Change link names

--- hallowe_frontend/hallowe_frontend/src/components/ViewSwitch.vue
+++ hallowe_frontend/hallowe_frontend/src/components/ViewSwitch.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex w-full justify-end px-4">
+  <div class="flex w-full justify-end px-4 pb-12 md:pb-0">
     <div class="relative w-30 h-12">
       <!-- Main pill shape -->
       <div class="absolute inset-0 border-2 border-[#ff7518] rounded-full"></div>
@@ -9,7 +9,7 @@
 
       <!-- Left circle -->
          <RouterLink to="/map">
-           <div class="absolute top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2" title="map view">
+           <div class="absolute top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2" title="map page">
             <span class="pi pi-map-marker text-[#ff7518] hover:text-white transition-colors duration-300 ease-in-out" :class="{ 'text-white': $route.path === '/map' }" style="font-size: 1.2rem"></span>
            </div>
           </RouterLink>
@@ -17,7 +17,7 @@
       <!-- Right circle -->
 
         <RouterLink to="/search">
-          <div class="absolute top-1/2 right-1/4 translate-x-1/2 -translate-y-1/2" title="list view">
+          <div class="absolute top-1/2 right-1/4 translate-x-1/2 -translate-y-1/2" title="list page">
             <span class="pi pi-list text-[#ff7518] hover:text-white transition-colors duration-300 ease-in-out" :class="{ 'text-white': $route.path === '/search' }" style="font-size: 1.2rem"></span>
           </div>
         </RouterLink>
