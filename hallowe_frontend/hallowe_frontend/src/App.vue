2a7c84241b692f2563fc89410bfd53d8b194521a
therealdanmalmx <danmalmx@gmail.com>
2026-01-08 17:34:15 +0100

    Add a view switch set of icons

--- hallowe_frontend/hallowe_frontend/src/App.vue
+++ hallowe_frontend/hallowe_frontend/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
   import Navbar from './components/NavbarComp.vue'
   import { RouterView } from 'vue-router'
+import ViewSwitch from './components/ViewSwitch.vue';
 </script>
 
 <template>
   <div class="py-6">
+    <ViewSwitch />
     <RouterView />
     <Navbar />
   </div>
---
+++ hallowe_frontend/hallowe_frontend/src/components/ViewSwitch.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex w-full justify-end px-4">
+    <div class="relative w-30 h-12">
+      <!-- Main pill shape -->
+      <div class="absolute inset-0 border-2 border-[#ff7518] rounded-full"></div>
+
+      <!-- Center divider -->
+      <div class="absolute top-0 bottom-0 left-1/2 w-0.5 bg-[#ff7518] -translate-x-1/2"></div>
+
+      <!-- Left circle -->
+         <RouterLink to="/map">
+           <div class="absolute top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2" title="map view">
+            <span class="pi pi-map-marker text-[#ff7518] hover:text-white transition-colors duration-300 ease-in-out" :class="{ 'text-white': $route.path === '/map' }" style="font-size: 1.2rem"></span>
+           </div>
+          </RouterLink>
+
+      <!-- Right circle -->
+
+        <RouterLink to="/search">
+          <div class="absolute top-1/2 right-1/4 translate-x-1/2 -translate-y-1/2" title="list view">
+            <span class="pi pi-list text-[#ff7518] hover:text-white transition-colors duration-300 ease-in-out" :class="{ 'text-white': $route.path === '/search' }" style="font-size: 1.2rem"></span>
+          </div>
+        </RouterLink>
+    </div>
+  </div>
+</template>
