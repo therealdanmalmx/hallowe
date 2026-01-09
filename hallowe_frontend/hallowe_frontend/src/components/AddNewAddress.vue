a717a7b6f2be05b4e57505ae2c9111998aac5992
therealdanmalmx <danmalmx@gmail.com>
2026-01-09 17:14:24 +0100

    Create Add New Address button

---
+++ hallowe_frontend/hallowe_frontend/src/components/AddNewAddress.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <div>
+      <div class="flex w-full justify-end" :class="{ 'pt-6': $route.path === '/search' }">
+          <RouterLink to="/add-address">
+            <button class="flex items-center">
+                <div class="pi pi-plus"></div>
+                <div class="px-2">Lägg till en ny address</div>
+            </button>
+          </RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
