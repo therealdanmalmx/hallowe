0bc944e3f64611793f4d36edebaf1a79ba669874
therealdanmalmx <danmalmx@gmail.com>
2026-01-14 14:53:50 +0100

    Add font and color

--- hallowe_frontend/hallowe_frontend/src/views/SearchView.vue
+++ hallowe_frontend/hallowe_frontend/src/views/SearchView.vue
@@ -43,7 +43,7 @@
   <div class="h-screen flex items-center justify-center" v-if="isLoading"><rotate-loader :loading="isLoading" :color="color" :size="size"></rotate-loader></div>
   <div v-else class="px-4 xl:px-0">
     <div class="h-calc(100vh_-_40px)">
-      <h1 class="text-5xl">List Page</h1>
+      <h1 class="text-5xl scary-font text-[#ff7518]">List Page</h1>
       <SearchComp />
     </div>
 
