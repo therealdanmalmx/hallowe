328f0003fa6f0ce9a1339ad829c0819fca6d5cbc
therealdanmalmx <danmalmx@gmail.com>
2026-04-11 13:19:29 +0200

    Add import of leaflet componet

--- hallowe_frontend/hallowe_frontend/src/views/MapView.vue
+++ hallowe_frontend/hallowe_frontend/src/views/MapView.vue
@@ -1,16 +1,19 @@
 <script lang="ts">
 
   import { defineComponent } from 'vue';
+
   import MapComp from '../components/MapComp.vue';
   import { useLocationStore } from '../stores/locationStore';
   import { useTimeSlotsStore } from '../stores/timeSlotsStore';
   import { useUserStore } from '../stores/userStore';
+  import LeafletMap from '../components/LeafletMap.vue';
 
   export default defineComponent({
     name: 'MapView',
-    components: { MapComp },
+    components: { MapComp, LeafletMap },
 
     setup() {
+
       const locationStore = useLocationStore();
       const timeSlotsStore = useTimeSlotsStore();
       const userStore = useUserStore();
@@ -26,8 +29,9 @@
 <template>
   <div class="w-full lg:w-11/12 mx-auto">
     <h1 class="text-5xl scary-font text-[#ff7518] text-center">Map Page</h1>
-    <div class="p-4 xl:p-0">
-        <MapComp />
+    <div class="p-4 xl:p-2">
+        <!-- <MapComp /> -->
+        <LeafletMap />
         <div v-if="locationStore.error" class="error-message">{{ locationStore.error }}</div>
         <div v-else-if="timeSlotsStore.error" class="error-message">{{ timeSlotsStore.error }}</div>
     </div>
