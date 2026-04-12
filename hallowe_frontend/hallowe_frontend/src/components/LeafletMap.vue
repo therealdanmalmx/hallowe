641030fd5ce22c66f73737d1d92f6000f4860f94
therealdanmalmx <danmalmx@gmail.com>
2026-04-12 20:39:52 +0200

    Get real data for map, location and ipopup

--- hallowe_frontend/hallowe_frontend/src/components/LeafletMap.vue
+++ hallowe_frontend/hallowe_frontend/src/components/LeafletMap.vue
@@ -4,22 +4,54 @@
     <rotate-loader :loading="!mapReady" :color="color" :size="size"></rotate-loader>
   </div>
   <div v-else class="h-[50vh] flex flex-col justify-center pt-8">
-     <l-map ref="map" v-model:zoom="zoom" :center="[57.723015, 12.946711]">
-      <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        layer-type="base"
-        name="OpenStreetMap"
-      ></l-tile-layer>
-      <l-marker :lat-lng="[57.723015, 12.946711]">
-        <l-icon
-            :icon-size="[35, 35]"
-            :icon-anchor="[20, 20]"
-            icon-url="/public/images/pumpkin.webp"
-        ></l-icon>
-        <l-popup options="open">Hello!</l-popup>
-      </l-marker>
+    <l-map
+      :zoom="currentZoom"
+      :center="currentCenter"
+    >
+    <l-tile-layer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      layer-type="base"
+      name="OpenStreetMap"
+    ></l-tile-layer>
+    <l-marker
+        v-for="user in locationStore.filteredLocations"
+        :key="user.id"
+        @click="openMarker(user.id ?? null)"
+        :lat-lng="[user.latitude, user.longitude]" >
+      <l-icon
+          :icon-size="[35, 35]"
+          :icon-anchor="[20, 20]"
+          icon-url="/public/images/pumpkin.webp"
+      ></l-icon>
+      <l-popup>
+        <div class="flex flex-col text-left justify-start">
+          <div class="text-black text-base">
+            <p class="font-bold">{{ user.name }}</p>
+            <span class="flex flex-col justify-start text-sm">
+              <p>{{ user.streetName }}, {{ user.streetNumber }}</p>
+              <p>{{ user.postalCode }} {{ user.city }}</p>
+            </span>
+          </div>
 
-    </l-map>
+          <div v-for="time in timeSlotsStore.times.filter(t => t.id === user.timeSlotId)">
+            <div class="flex flex-col space-y-2 mt-4">
+              <div class="flex items-center text-lg space-x-2 font-bold">
+                <i class="text-[#ff7518] pi pi-calendar "></i>
+                <p class="text-sm">{{time.date}}</p>
+              </div>
+              <div class="flex items-center text-lg space-x-2 font-bold">
+                <i class="text-[#ff7518] pi pi-clock"></i>
+                <p class="text-sm">{{ time.startTime.slice(0, 5) }} - {{ time.endTime.slice(0, 5) }}</p>
+              </div>
+              <!-- <div class="border-t border-gray-300 mt-2 pt-1.5 text-[13px] overflow-hidden whitespace-nowrap text-ellipsis font-['Roboto',Arial]">
+                <a class="googel-maps-links" :href="`https://www.google.com/maps?q=${user.latitude},${user.longitude}&z=15`" target="_blank">Se på Google Maps</a>
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </l-popup>
+    </l-marker>
+  </l-map>
   </div>
   <div v-if="!locationStore.isLoading">
     <SearchComp />
@@ -27,46 +59,84 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
     import "leaflet/dist/leaflet.css";
     import { LMap, LMarker, LPopup, LTileLayer, LIcon } from "@vue-leaflet/vue-leaflet";
-    import { defineComponent, onMounted, ref } from "vue";
+    import { computed, defineComponent, onMounted, ref } from "vue";
     import { useLocationStore } from '../stores/locationStore';
     import { useTimeSlotsStore } from '../stores/timeSlotsStore';
     import { useUserStore } from '../stores/userStore';
     import AddNewAddress from './AddNewAddress.vue';
     import SearchComp from './SearchComp.vue';
+    import { getUserCoords, defaultCenter } from '../utils/getUserCoordinates';
     import RotateLoader from 'vue-spinner/src/RotateLoader.vue';
 
-
     export default defineComponent({
-        name: 'MapView',
-        components: {
-            LMap,
-            LTileLayer,
-            LMarker,
-            LIcon,
-            LPopup,
-            AddNewAddress,
-            SearchComp,
-            RotateLoader
-        },
+      name: 'MapView',
+      components: {
+          LMap,
+          LTileLayer,
+          LMarker,
+          LIcon,
+          LPopup,
+          AddNewAddress,
+          SearchComp,
+          RotateLoader
+      },
 
-        setup() {
+      setup() {
 
-            const zoom = ref(15);
-            const mapReady = ref(false);
-            const locationStore = useLocationStore();
-            const timeSlotsStore = useTimeSlotsStore();
-            const userStore = useUserStore();
+        const zoom = ref(15);
+        const openedMarkerID = ref<number>(0);
+        const mapReady = ref<boolean>(false);
+        const color = ref<string>("#FF7518");
+        const size = ref<string>('1.25rem');
+        const baseZoom = ref<number>(window.innerWidth < 768 ? 4 : 6);
+        const locationStore = useLocationStore();
+        const timeSlotsStore = useTimeSlotsStore();
+        const userStore = useUserStore();
 
-            onMounted(async () => {
-                await import("leaflet");
-                mapReady.value = true;
-            });
+        onMounted(async () => {
+          getUserCoords();
+          await import("leaflet");
+          await locationStore.getAllParticiants();
+          await timeSlotsStore.getAllTimeSlots();
+          mapReady.value = true;
+        });
 
-            return { locationStore, timeSlotsStore, userStore, zoom, mapReady }
+        const openMarker = (id: number | null) => {
+          openedMarkerID.value = id ?? 0;
         }
+
+        const currentCenter = computed((): [number, number] => {
+          const center = locationStore.mapCenter || defaultCenter.value;
+          return [center.lat, center.lng];
+        });
+
+        const currentZoom = computed(() => {
+          return locationStore.mapZoom || baseZoom.value;
+        });
+
+        return {
+          locationStore,
+          timeSlotsStore,
+          userStore,
+          zoom,
+          mapReady,
+          openedMarkerID,
+          openMarker,
+          color,
+          size,
+          currentCenter,
+          currentZoom
+        }
+      }
     })
 
 </script>
+
+<style>
+  .leaflet-popup-content p {
+    margin: 0;
+  }
+</style>
