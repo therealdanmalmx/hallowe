f277dc38a5d585649e7f9eb68db8180e4405230c
therealdanmalmx <danmalmx@gmail.com>
2026-04-12 20:43:12 +0200

    Remove google map

--- hallowe_frontend/hallowe_frontend/src/views/MapView.vue
+++ hallowe_frontend/hallowe_frontend/src/views/MapView.vue
@@ -2,7 +2,6 @@
 
   import { defineComponent } from 'vue';
 
-  import MapComp from '../components/MapComp.vue';
   import { useLocationStore } from '../stores/locationStore';
   import { useTimeSlotsStore } from '../stores/timeSlotsStore';
   import { useUserStore } from '../stores/userStore';
@@ -10,7 +9,7 @@
 
   export default defineComponent({
     name: 'MapView',
-    components: { MapComp, LeafletMap },
+    components: { LeafletMap },
 
     setup() {
 
@@ -30,7 +29,6 @@
   <div class="w-full lg:w-11/12 mx-auto">
     <h1 class="text-5xl scary-font text-[#ff7518] text-center">Karta</h1>
     <div class="p-4 xl:p-2">
-        <!-- <MapComp /> -->
         <LeafletMap />
         <div v-if="locationStore.error" class="error-message">{{ locationStore.error }}</div>
         <div v-else-if="timeSlotsStore.error" class="error-message">{{ timeSlotsStore.error }}</div>
