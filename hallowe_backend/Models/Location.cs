27db7692442c014b9d8a5dade63060f34fe96f97
therealdanmalmx <danmalmx@gmail.com>
2026-04-20 16:59:45 +0200

    Move timeslot into location

--- hallowe_backend/Models/Location.cs
+++ hallowe_backend/Models/Location.cs
@@ -3,15 +3,16 @@ namespace hallowe_backend.Models
 {
     public class Location
     {
-        public Guid Id { get; set; } = Guid.CreateVersion7();
+        public Guid Id { get; set; }
         public required string StreetName { get; set; } = string.Empty;
         public required string StreetNumber { get; set; } = string.Empty;
         public required string City { get; set; } = string.Empty;
         public required string PostalCode { get; set; } = string.Empty;
         public required decimal Latitude { get; set; }
         public required decimal Longitude { get; set; }
-        public required TimeSlot TimeSlots { get; set; }
-        public Guid TimeSlotId { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }        
         public required string UserId { get; set; } = default!;
         [JsonIgnore]
         public User? User { get; set; } = default!;
