7299d9eb5c3e46083026bcaf3bc9e4d5cb9109e0
therealdanmalmx <danmalmx@gmail.com>
2026-07-18 17:42:05 +0100

    Remove token from response

--- hallowe_backend/Models/Login/LoginResponse.cs
+++ hallowe_backend/Models/Login/LoginResponse.cs
@@ -4,19 +4,12 @@ namespace hallowe_backend.Models.Login
     {
         public bool IsSuccessful { get; set; }
         public string? Errors { get; set; } = null;
-        public string? Token { get; set; } = null;
 
         public LoginResponse() { }
         public LoginResponse(bool isSuccessful, string? errors)
         {
             IsSuccessful = isSuccessful;
             Errors = errors;
-        }
-        public LoginResponse(bool isSuccessful, string? errors, string? token)
-        {
-            IsSuccessful = isSuccessful;
-            Errors = errors;
-            Token = token;
         }
     }
 }
