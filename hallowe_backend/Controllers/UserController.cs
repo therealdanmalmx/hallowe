62f351b5e39992f71f2987c282483d98b38e32c8
therealdanmalmx <danmalmx@gmail.com>
2026-07-18 12:15:45 +0100

    Add login and specify routes

--- hallowe_backend/Controllers/UserController.cs
+++ hallowe_backend/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using hallowe_backend.DTOs;
+using hallowe_backend.Models.Login;
 using hallowe_backend.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,12 @@ namespace hallowe_backend.Controllers
     public class UserController : ControllerBase
     {
         private readonly IRegisterService _registerService;
+        private readonly IloginService _loginService;
 
-        public UserController(IRegisterService registerService)
+        public UserController(IRegisterService registerService, IloginService loginService)
         {
             _registerService = registerService;
+            _loginService = loginService;
         }
 
         [HttpGet]
@@ -22,14 +25,27 @@ namespace hallowe_backend.Controllers
             return Ok(result);
         }
 
-        [HttpPost]
-        public async Task<ActionResult<RegisterResponse>> RegisterParticipant(RegisterRequest request)
+        [HttpPost("register")]
+        public async Task<ActionResult<RegisterResponse>> Register(RegisterRequest request)
         {
             var result = await _registerService.RegisterUser(request);
 
             if (!result.IsSuccessful)
             {
                 return BadRequest(new RegisterResponse(false, result.Errors));
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponse>> Login(LoginRequest request)
+        {
+            var result = await _loginService.Login(request);
+
+            if (!result.IsSuccessful)
+            {
+                return new LoginResponse(false, result.Errors);
             }
 
             return Ok(result);
