24829fa060ff473b11a3126a23c7c49c823ec1ce
therealdanmalmx <danmalmx@gmail.com>
2026-07-18 12:16:37 +0100

    Remove GooglMaps loader

--- hallowe_frontend/hallowe_frontend/src/main.ts
+++ hallowe_frontend/hallowe_frontend/src/main.ts
@@ -2,18 +2,18 @@ import VueGoogleMaps from '@fawmi/vue-google-maps';
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import {router} from "./router";
 import "./style.css";
 
 createApp(App)
   .use(router)
   .use(createPinia())
-  .use(VueGoogleMaps, {
-    load: {
-      key: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-      libraries: 'places',
-      await: true,
-    },
-  })
+  // .use(VueGoogleMaps, {
+  //   load: {
+  //     key: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+  //     libraries: 'places',
+  //     await: true,
+  //   },
+  // })
 
 .mount("#app");
