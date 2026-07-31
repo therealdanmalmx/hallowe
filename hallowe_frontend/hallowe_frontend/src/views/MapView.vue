75ca68c963374b233d9ef0d9365867353e141960
therealdanmalmx <danmalmx@gmail.com>
2026-07-31 22:10:36 +0100

    remove padding on large screens

--- hallowe_frontend/hallowe_frontend/src/views/MapView.vue
+++ hallowe_frontend/hallowe_frontend/src/views/MapView.vue
@@ -26,7 +26,7 @@
 <template>
   <div class="w-full lg:w-11/12 mx-auto">
     <h1 class="text-5xl scary-font text-[#ff7518] text-center">Karta</h1>
-    <div class="p-4 xl:p-2">
+    <div class="p-4 md:p-0">
         <LeafletMap />
         <div v-if="locationStore.error" class="error-message">{{ locationStore.error }}</div>
     </div>
