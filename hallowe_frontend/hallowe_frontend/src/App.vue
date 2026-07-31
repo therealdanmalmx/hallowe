afcbd3d653b2daed8f1d2fa8467baa2446611c1b
therealdanmalmx <danmalmx@gmail.com>
2026-07-31 22:09:51 +0100

    Remove logout test

--- hallowe_frontend/hallowe_frontend/src/App.vue
+++ hallowe_frontend/hallowe_frontend/src/App.vue
@@ -1,28 +1,11 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router';
   import ViewSwitch from './components/ViewSwitch.vue';
-
-  const logout = async () => {
-    console.log("logged put")
-    const loggedOut = await fetch("http://localhost:5168/api/user/logout", {
-        method: "POST",
-        credentials: "include",
-      });
-
-      console.log({loggedOut})
-      if (loggedOut.ok)
-      {
-        window.location.href = '/map'
-      }
-  }
 </script>
 
 <template>
     <div v-if="$route.path != '/' && $route.path != '/login' && $route.path != '/register'" class="py-6 w-11/12 mx-auto">
       <ViewSwitch  />
-    </div>
-    <div @click="() => logout()">
-      Logout
     </div>
     <RouterView />
 </template>
