6ad111a44657c6df3cd6e814bb14ffbcb36b5c6f
therealdanmalmx <danmalmx@gmail.com>
2026-07-31 21:51:21 +0100

    Refactor model

--- hallowe_backend/Models/Location.cs
+++ hallowe_backend/Models/Location.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 namespace hallowe_backend.Models
 {
     public class Location
     {
         public Guid Id { get; set; }
+        [MaxLength(35)]
+        public required string Name { get; set; } = string.Empty;
+        [MaxLength(25)]
         public required string StreetName { get; set; } = string.Empty;
+        [MaxLength(3)]
         public required string StreetNumber { get; set; } = string.Empty;
+        [MaxLength(20)]
         public required string City { get; set; } = string.Empty;
+        [MaxLength(6)]
         public required string PostalCode { get; set; } = string.Empty;
         public required decimal Latitude { get; set; }
         public required decimal Longitude { get; set; }
@@ -16,10 +23,5 @@ namespace hallowe_backend.Models
         public required string UserId { get; set; } = default!;
         [JsonIgnore]
         public User? User { get; set; } = default!;
-        public string Name
-        {
-            get => User?.Name ?? string.Empty;
-            private set {}
-        }
     }
 }
