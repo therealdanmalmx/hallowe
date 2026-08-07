1103a2e64c5a38ff4185b2eefc135d6342e340b8
therealdanmalmx <danmalmx@gmail.com>
2026-08-07 15:59:32 +0200

    Add DTO's

---
+++ hallowe_backend/DTOs/UpdateLocationRequest.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using hallowe_backend.Models;
+
+namespace hallowe_backend.DTOs;
+
+public class AddNewLocation
+{
+    [MaxLength(35)]
+    public required string Name { get; set; }
+    [MaxLength(25)]
+    public required string StreetName { get; set; }
+    [MaxLength(10)]
+    public required string StreetNumber { get; set; }
+    [MaxLength(20)]
+    public required string City { get; set; }
+    [MaxLength(6)]
+    [RegularExpression(@"^\d{3} ?\d{2}$", ErrorMessage = "Postnummer måste ha 5 siffror. Separera dem med ett mellanslag (nnn nn")]
+    public required string PostalCode { get; set; }
+    public required decimal Latitude { get; set; }
+    public required decimal Longitude { get; set; }
+    public required bool TrickOrTreat { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }        
+}
