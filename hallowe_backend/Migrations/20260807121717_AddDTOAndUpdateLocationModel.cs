2acbdfe8411cdcfc9a1121791bf335bc455bc518
therealdanmalmx <danmalmx@gmail.com>
2026-08-07 16:07:44 +0200

    Migration

---
+++ hallowe_backend/Migrations/20260807121717_AddDTOAndUpdateLocationModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hallowe_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDTOAndUpdateLocationModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StreetNumber",
+                table: "Locations",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(3)",
+                oldMaxLength: 3);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TrickOrTreat",
+                table: "Locations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrickOrTreat",
+                table: "Locations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StreetNumber",
+                table: "Locations",
+                type: "character varying(3)",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+        }
+    }
+}
