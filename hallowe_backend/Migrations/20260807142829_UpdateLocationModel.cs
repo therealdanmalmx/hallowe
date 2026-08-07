ca85ad367d54126fbde67d1f8dd4bdd643a4a51d
therealdanmalmx <danmalmx@gmail.com>
2026-08-07 17:17:06 +0200

    migratio

---
+++ hallowe_backend/Migrations/20260807142829_UpdateLocationModel.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hallowe_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateLocationModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Longitude",
+                table: "Locations",
+                type: "numeric(9,6)",
+                precision: 9,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Latitude",
+                table: "Locations",
+                type: "numeric(9,6)",
+                precision: 9,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Longitude",
+                table: "Locations",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(9,6)",
+                oldPrecision: 9,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Latitude",
+                table: "Locations",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(9,6)",
+                oldPrecision: 9,
+                oldScale: 6);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
