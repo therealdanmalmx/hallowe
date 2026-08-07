98e8d487199a29e1d32d15b040d51f468351dd8c
therealdanmalmx <danmalmx@gmail.com>
2026-08-07 16:02:28 +0200

    Add trickortreat prop. Change length for StreetNumber.

--- hallowe_backend/Models/Location.cs
+++ hallowe_backend/Models/Location.cs
@@ -6,20 +6,21 @@ namespace hallowe_backend.Models
     {
         public Guid Id { get; set; }
         [MaxLength(35)]
-        public required string Name { get; set; } = string.Empty;
+        public required string Name { get; set; }
         [MaxLength(25)]
-        public required string StreetName { get; set; } = string.Empty;
-        [MaxLength(3)]
-        public required string StreetNumber { get; set; } = string.Empty;
+        public required string StreetName { get; set; }
+        [MaxLength(10)]
+        public required string StreetNumber { get; set; }
         [MaxLength(20)]
-        public required string City { get; set; } = string.Empty;
+        public required string City { get; set; }
         [MaxLength(6)]
-        public required string PostalCode { get; set; } = string.Empty;
+        public required string PostalCode { get; set; }
         public required decimal Latitude { get; set; }
         public required decimal Longitude { get; set; }
+        public required bool TrickOrTreat { get; set; }
         public DateOnly Date { get; set; }
         public TimeOnly StartTime { get; set; }
-        public TimeOnly EndTime { get; set; }        
+        public TimeOnly EndTime { get; set; }
         public required string UserId { get; set; } = default!;
         [JsonIgnore]
         public User? User { get; set; } = default!;
