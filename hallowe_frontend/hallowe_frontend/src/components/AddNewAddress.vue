6870e0b209d1d14ade5dffc7d467fc3f926a4cc0
therealdanmalmx <danmalmx@gmail.com>
2026-08-07 16:04:18 +0200

    Switch button depending if user is Autheorized

--- hallowe_frontend/hallowe_frontend/src/components/AddNewAddress.vue
+++ hallowe_frontend/hallowe_frontend/src/components/AddNewAddress.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="flex w-full justify-end" :class="{ 'pt-6': $route.path === '/search' }">
       <RouterLink to="/add-address">
-        <button class="flex items-center">
+        <button v-if="isAuthenticated" class="flex items-center">
+            <div class="pi pi-pencil"></div>
+            <div class="px-2">Uppdatera address</div>
+        </button>
+        <button v-else class="flex items-center">
             <div class="pi pi-plus"></div>
             <div class="px-2">Lägg till en ny address</div>
         </button>
       </RouterLink>
   </div>
 </template>
+
+<script setup lang="ts">
+  import { useUserStore } from "../stores/userStore.ts"
+
+  const { isAuthenticated } = useUserStore();
+</script>
