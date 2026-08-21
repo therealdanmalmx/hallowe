f5bf7a5a27d0bacc744e88c0041680daeecda13f
therealdanmalmx <danmalmx@gmail.com>
2026-08-21 10:17:14 +0200

    Remove delete from user.

--- hallowe_frontend/hallowe_frontend/src/api/services/userService.ts
+++ hallowe_frontend/hallowe_frontend/src/api/services/userService.ts
@@ -14,13 +14,10 @@ export const userService = {
     login(user: User): Promise<any> {
         return useHttpClient().post('/user/login', user)
     },
-logout() {
-    return useHttpClient().post('/user/logout')
-},
+    logout() {
+        return useHttpClient().post('/user/logout')
+    },
     update(id: string | number, user: User): Promise<any> {
         return useHttpClient().put(`/user/${id}`, user)
-    },
-    delete(id: string | number): Promise<any> {
-        return useHttpClient().delete(`/user/${id}`)
     }
 }
