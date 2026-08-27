dd79bf9baa7ee4d76a82af58c7dc642490b2ac02
therealdanmalmx <danmalmx@gmail.com>
2026-08-27 20:16:48 +0200

    Register vue-toastification

--- hallowe_frontend/hallowe_frontend/src/main.ts
+++ hallowe_frontend/hallowe_frontend/src/main.ts
@@ -1,12 +1,18 @@
-import VueGoogleMaps from '@fawmi/vue-google-maps';
+import Toast, { type PluginOptions } from "vue-toastification";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 import {router} from "./router";
+import "vue-toastification/dist/index.css";
 import "./style.css";
+
+const options: PluginOptions = {
+    // You can set your default options here
+};
 
 createApp(App)
   .use(router)
   .use(createPinia())
+  .use(Toast, options)
 
 .mount("#app");
