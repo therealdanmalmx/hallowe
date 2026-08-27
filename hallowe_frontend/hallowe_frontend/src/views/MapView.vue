4bce8f7912b9b38b58c027ff95675a307ecb72c3
therealdanmalmx <danmalmx@gmail.com>
2026-08-27 20:20:39 +0200

    Remove error message in lieu of toast.

--- hallowe_frontend/hallowe_frontend/src/views/MapView.vue
+++ hallowe_frontend/hallowe_frontend/src/views/MapView.vue
@@ -2,9 +2,9 @@
 
   import { defineComponent } from 'vue';
 
-  import { useLocationStore } from '../stores/locationStore';
-  import { useUserStore } from '../stores/userStore';
   import LeafletMap from '../components/LeafletMap.vue';
+import { useLocationStore } from '../stores/locationStore';
+import { useUserStore } from '../stores/userStore';
 
   export default defineComponent({
     name: 'MapView',
@@ -28,7 +28,6 @@
     <h1 class="text-5xl scary-font text-[#ff7518] text-center">Karta</h1>
     <div class="p-4 md:p-0">
         <LeafletMap />
-        <div v-if="locationStore.error" class="error-message">{{ locationStore.error }}</div>
     </div>
 
   </div>
