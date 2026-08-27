1066f3715c4dba66d8588ddbf338f8adb2ff8173
therealdanmalmx <danmalmx@gmail.com>
2026-08-27 20:58:20 +0200

    Cross eye when hiding password

--- hallowe_frontend/hallowe_frontend/src/views/Register.vue
+++ hallowe_frontend/hallowe_frontend/src/views/Register.vue
@@ -40,7 +40,7 @@
                 required
               />
               <button @click="showPassword = !showPassword" type="button" class="absolute right-3 top-1/2 -translate-y-1/2 text-orange-400 hover:text-orange-300">
-                <svg v-if="!showPassword" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <svg v-if="showPassword" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                 </svg>
