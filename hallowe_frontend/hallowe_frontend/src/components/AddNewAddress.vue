d61a1f3cd9b4da5d4d3a45df03a3f5f52da21ee3
therealdanmalmx <danmalmx@gmail.com>
2026-08-29 21:32:48 +0200

    Check if user has a location

--- hallowe_frontend/hallowe_frontend/src/components/AddNewAddress.vue
+++ hallowe_frontend/hallowe_frontend/src/components/AddNewAddress.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="flex w-full justify-end" :class="{ 'pt-6': route.path === '/search' }">
-      <RouterLink to="/add-address">
-        <button v-if="isAuthenticated" class="flex items-center">
+      <RouterLink to="/add-address" class="text-slate-200!">
+        <button v-if="isAuthenticated && userHasLocation()" class="flex items-center">
             <div class="pi pi-pencil"></div>
             <div class="px-2">Uppdatera address</div>
         </button>
-        <button v-else class="flex items-center" @click="sendToAddress()"">
+        <button v-else-if="!userHasLocation()" class="flex items-center" @click="goToAddress()">
+            <div class="pi pi-plus"></div>
+            <div class="px-2">Lägg till en ny address</div>
+        </button>
+        <button v-else class="flex items-center" @click="sendToAddress()">
             <div class="pi pi-plus"></div>
             <div class="px-2">Lägg till en ny address</div>
         </button>
@@ -17,10 +21,22 @@
   import { useRoute } from "vue-router"
   import { useUserStore } from "../stores/userStore.ts"
   import { useToast } from "vue-toastification";
+  import { useLocationStore } from "../stores/locationStore.ts";
+import { router } from "../router.ts";
   
-  const { isAuthenticated } = useUserStore();
+  const { isAuthenticated, userId } = useUserStore();
+  const { locations } = useLocationStore();
+
+  const userHasLocation = () => {
+    return locations.find((l) => l.userId === userId);
+  }
+
   const route = useRoute();
   const toast = useToast();
+
+  const goToAddress = () => {
+    router.push("/add-address")
+  }
 
   const sendToAddress = () => {
     toast.info("Du måste vara inloggad för att lägga till en address", {timeout: 3000})
